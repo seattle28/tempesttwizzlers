1f8673077c05f2852918b7423c580b83cfb7f447
seattle28 <148305617+seattle28@users.noreply.github.com>
2023-12-29 06:44:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const starField = document.getElementById("stars");
+
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.left = Math.random() * 100 + "vw";
+        star.style.animationDuration = Math.random() * 1 + 1 + "s";
+        starField.appendChild(star);
+    }
+});
